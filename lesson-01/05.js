dc4941a439dd1775e565e30919cbf5607eb546ee
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-08 09:24:04 +0300

    add switch

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -3,12 +3,12 @@ let length = rule.length;
 
 switch (true) {
     case length < 25:
-        console.log("Все таки нет правил без исключения");
+        console.log("50 на 50");
         break;
     case length > 25:
         console.log("У меня 100 пудов все получится");
         break;
     case length === 25:
-        console.log("50 на 50");
+        console.log("Все таки нет правил без исключения");
         break;
 }
