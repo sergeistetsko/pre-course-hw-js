6504523969cab211a745ff3c19cb0a3523687ac9
sergeistetsko <sergeistetsko2021@yandex.by>
2023-11-24 14:15:32 +0300

    add 03/03

---
+++ lesson-03/03.js
@@ -0,0 +1,4 @@
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+
+let slicedFruits = fruits.slice(1, fruits.length - 2);
+console.log(slicedFruits);
